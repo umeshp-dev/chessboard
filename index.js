67a8a1d3908b02b412c200e031d02d2d9bbf1f7d
umesh patro <umeshpatro90@gmail.com>
2022-04-22 00:45:44 +0530

    commit

---
+++ index.js
@@ -0,0 +1,87 @@
+let mainContainer = document.getElementById("mainContainer")
+
+for(let i = 1; i < 9; i++){
+   let box1 = document.createElement("div")
+    if(i%2==0){
+        box1.style.backgroundColor = 'black';
+    }else{
+        box1.style.backgroundColor = "white";
+    }
+    mainContainer.appendChild(box1);
+}
+
+for(let i = 1; i < 9; i++){
+    let box2 = document.createElement("div")
+     if(i%2==0){
+         box2.style.backgroundColor = 'white';
+     }else{
+         box2.style.backgroundColor = "black";
+     }
+     mainContainer.appendChild(box2);
+ }
+
+ for(let i = 1; i < 9; i++){
+    let box3 = document.createElement("div")
+     if(i%2==0){
+         box3.style.backgroundColor = 'black';
+     }else{
+         box3.style.backgroundColor = "white";
+     }
+     mainContainer.appendChild(box3);
+ }
+
+
+ for(let i = 1; i < 9; i++){
+    let box4 = document.createElement("div")
+     if(i%2==0){
+         box4.style.backgroundColor = 'white';
+     }else{
+         box4.style.backgroundColor = "black";
+     }
+     mainContainer.appendChild(box4);
+ }
+
+
+ for(let i = 1; i < 9; i++){
+    let box5 = document.createElement("div")
+     if(i%2==0){
+         box5.style.backgroundColor = 'black';
+     }else{
+         box5.style.backgroundColor = "white";
+     }
+     mainContainer.appendChild(box5);
+ }
+
+
+ for(let i = 1; i < 9; i++){
+    let box6 = document.createElement("div")
+     if(i%2==0){
+         box6.style.backgroundColor = 'white';
+     }else{
+         box6.style.backgroundColor = "black";
+     }
+
+     mainContainer.appendChild(box6);
+ }
+
+
+ for(let i = 1; i < 9; i++){
+    let box7 = document.createElement("div")
+     if(i%2==0){
+         box7.style.backgroundColor = 'black';
+     }else{
+         box7.style.backgroundColor = "white";
+     }
+     mainContainer.appendChild(box7);
+ }
+
+
+ for(let i = 1; i < 9; i++){
+    let box8 = document.createElement("div")
+     if(i%2==0){
+         box8.style.backgroundColor = 'white';
+     }else{
+         box8.style.backgroundColor = "black";
+     }
+     mainContainer.appendChild(box8);
+ }
